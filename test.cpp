96f6381d04815a010d98435e8767662639ec4c13
hdsxwt <xuewentao3416@163.com>
2024-08-09 16:46:55 +0800

    testing

--- test.cpp
+++ test.cpp
@@ -1,11 +1,24 @@
-#include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
+#include"Screen_element_lib.h"
+
 
 int main() {
-	for (int i = 0; i < 100; i++) {
-		putchar('+');
-		putchar('\n');
-		putchar('\n');
+	Button button1;
+	button1.set_visible(true);
+	button1.set_text("button1");
+	button1.set_id(1);
+	Button button2;
+	button2.set_visible(true);
+	button2.set_text("button2");
+	button1.set_id(2);
+	root.add_son(&button1);
+	root.add_son(&button2);
+	
+	screen_element_controller.start();
+	while (1) {
+		Call_back call_back = root.update();
 	}
 }
+
+
